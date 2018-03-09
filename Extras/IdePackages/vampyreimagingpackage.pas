c04577b7968a10a618204de88eaac2f4218db2de
wp-xyz <wp_xxyyzzz@gmx.net>
2018-03-09 23:32:58 +0100

    Splitt off package VampyreImagingPackage_Extras from main package

--- Extras/IdePackages/vampyreimagingpackage.pas
+++ Extras/IdePackages/vampyreimagingpackage.pas
@@ -2,23 +2,23 @@
   This source is only used to compile and install the package.
  }
 
-unit VampyreImagingPackage; 
+unit VampyreImagingPackage;
 
+{$warn 5023 off : no warning about unused units}
 interface
 
 uses
-    Imaging, ImagingBitmap, ImagingCanvases, ImagingClasses, 
-  ImagingComponents, ImagingDds, ImagingFormats, ImagingIO, ImagingJpeg, 
-  ImagingNetworkGraphics, ImagingTarga, ImagingTypes, ImagingUtility, 
-  ImagingPortableMaps, ImagingExtras, ImagingGif, ImagingColors, 
-  ImagingRadiance, LazarusPackageIntf;
+  Imaging, ImagingBitmap, ImagingCanvases, ImagingClasses, ImagingComponents, 
+  ImagingDds, ImagingFormats, ImagingIO, ImagingJpeg, ImagingNetworkGraphics, 
+  ImagingTarga, ImagingTypes, ImagingUtility, ImagingPortableMaps, ImagingGif, 
+  ImagingColors, ImagingRadiance, LazarusPackageIntf;
 
 implementation
 
-procedure Register; 
+procedure Register;
 begin
-end; 
+end;
 
 initialization
-  RegisterPackage('VampyreImagingPackage', @Register); 
+  RegisterPackage('VampyreImagingPackage', @Register);
 end.
